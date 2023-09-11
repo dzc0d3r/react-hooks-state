ad80075caeae22ada2051b04c3224c06cedb4e9f
dzc0d3r <m4dcod3r@protonmail.com>
2023-09-11 16:00:08 +0100

    Finished assignement :tada:

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, {useState} from "react"
 
-function Counter() {
-  const [count, setCount] = useState(0);
+function Counter () {
+  const [count, setCount] = useState(0)
 
-  function increment() {
-    setCount(count + 1);
+  function increment () {
+    /** this will not work because of how setState works and its nature */
+    /*    
+     setCount(count + 1)
+     setCount(count + 1) 
+        
+    */
+    /** A usefull tip for scenarios like these is to use an anon arrow function */
+    setCount((prev) => prev + 1)
+    setCount((prev) => prev + 1)
+    console.log("Coool!")
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <button onClick={increment}>I have been clicked {count} times</button>
 }
 
-export default Counter;
+export default Counter
